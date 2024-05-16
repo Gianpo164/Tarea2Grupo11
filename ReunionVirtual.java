14d14127278b1937ff9b3f815d37454a1c423e26
Bliclk <iperez2023@udec.cl>
2024-05-16 00:27:46 -0200

    Creacion reunion virutal

---
+++ ReunionVirtual.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ReunionVirtual extends Reunion {
+    private String enlace;
+    public ReunionVirtual(Date f, Instant horaP, Duration duracionP, int tipo, ArrayList<Empleado> listaDeE,String enl){
+        super(f, horaP, duracionP, tipo, listaDeE);
+        enlace = enl;
+    }
+    public String getEnlace(){
+        return enlace;
+    }
+}
