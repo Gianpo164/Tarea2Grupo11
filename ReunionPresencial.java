5750c972327e7ccefb33ad2c879f1205314aa0c7
Bliclk <iperez2023@udec.cl>
2024-05-16 00:29:45 -0200

    Creacion reunion presencial

---
+++ ReunionPresencial.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ReunionPresencial extends Reunion {
+    private String sala;
+    public ReunionPresencial(Date f, Instant horaP, Duration duracionP, int tipo, ArrayList<Empleado> listaDeE,String s){
+        super(f, horaP, duracionP, tipo, listaDeE);
+        sala = s;
+    }
+    public String getEnlace(){
+        return sala;
+    }
+}
