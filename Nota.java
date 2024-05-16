967f09829ff354e2cc84daf00c5d03635ff15a1b
Gianpo164 <gmassoglia2023@udec.cl>
2024-05-16 02:54:53 -0400

    Documentación de clases Nota y tipoReunion

--- Nota.java
+++ Nota.java
@@ -1,8 +1,15 @@
 package org.example;
 
+/**
+ * Representación de las notas que pueden ser creadas en una reunión
+ */
 public class Nota {
     private String contenido;
 
+    /**
+     * Crea una nota a partir del texto introducido
+     * @param nota Texto que será guardado
+     */
     public Nota(String nota){
         if (!nota.isBlank()){
             contenido = nota;
